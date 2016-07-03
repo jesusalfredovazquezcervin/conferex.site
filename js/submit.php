dd36f5ff7ec195d5783900b21f44e091b9098320
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-02 22:29:45 -0500

    se agrega el campo company al formulario y al submit

--- js/submit.php
+++ js/submit.php
@@ -12,12 +12,13 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+$company = $_POST['company'];
+
 // Create the email and send the message
 $to = 'conferexmx@gmail.com'; // Add your email address in between the '' replacing jennpereira13@gmail.com - This is where the form will send a message to.
 //$cc = 'jesuscervin@icloud.com';
 $email_subject = "Conferex Forma de contacto:  $name";
-$email_body = "Has recibido un nuevo mensaje a traves de la forma de contacto del sitio de Conferex.mx.\n\n"."A continuacion los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
+$email_body = "Has recibido un nuevo mensaje a traves de la forma de contacto del sitio de Conferex.mx.\n\n"."A continuacion los detalles:\n\nNombre: $name\n\nEmpresa:\n$company\n\nEmail: $email_address\n\nMensaje:\n$message";
 $headers = "From: noreply@conferex.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
